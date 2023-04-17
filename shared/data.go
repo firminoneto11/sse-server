83e509891dc0c1174e173d23e6076021973b3ee1
Firmino Neto <firminoneto11@hotmail.com>
2023-04-16 21:15:28 -0300

    Chore: Added some debug statements and created a generic function

--- shared/data.go
+++ shared/data.go
@@ -1,8 +1,32 @@
 package shared
 
 import (
+	"fmt"
 	"sync"
 )
+
+// This generic function loops through the slice and compares each element in it with the element passed to the second parameter.
+// Removes the first occurrence found and returns a new slice without the element.
+func removeFromArray[T comparable](slice []T, element T) []T {
+	sliceWithoutElement := make([]T, 0)
+	alreadyFound := false
+	for _, value := range slice {
+		if value == element {
+			if alreadyFound {
+				sliceWithoutElement = append(sliceWithoutElement, value)
+				continue
+			}
+			alreadyFound = true
+		} else {
+			sliceWithoutElement = append(sliceWithoutElement, value)
+		}
+	}
+	return sliceWithoutElement
+}
+
+func (c *Client) display(key int) {
+	fmt.Printf("Amount of currently active channels for client %d: %d\n\n", key, len(c.Channels))
+}
 
 type Event struct {
 	Name string
@@ -13,7 +37,6 @@ type Client struct {
 	Channels []*chan Event
 }
 
-// This function can be used to craft the new ConnectedClients instances
 func NewConnectedClients() ConnectedClients {
 	return ConnectedClients{
 		clients: make(map[int]*Client),
@@ -46,6 +69,13 @@ func (cc *ConnectedClients) ConnectClient(id int, channel *chan Event) {
 		client := cc.clients[id]
 		client.Channels = append(client.Channels, channel)
 
+		// NOTE: Debug purposes
+		fmt.Printf("Amount of different clients connected: %d\n", len(cc.clients))
+		for key, value := range cc.clients {
+			value.display(key)
+		}
+		// NOTE: Debug purposes
+
 		return
 	}
 
@@ -56,6 +86,13 @@ func (cc *ConnectedClients) ConnectClient(id int, channel *chan Event) {
 	client.Channels = append(client.Channels, channel)
 
 	cc.clients[id] = &client
+
+	// NOTE: Debug purposes
+	fmt.Printf("Amount of different clients connected: %d\n", len(cc.clients))
+	for key, value := range cc.clients {
+		value.display(key)
+	}
+	// NOTE: Debug purposes
 }
 
 func (cc *ConnectedClients) DisconnectClient(id int, channel *chan Event) {
@@ -65,16 +102,25 @@ func (cc *ConnectedClients) DisconnectClient(id int, channel *chan Event) {
 	client, ok := cc.clients[id]
 
 	if ok {
-		for index, ch := range client.Channels {
-			if ch == channel {
-				client.Channels = append(client.Channels[:index], client.Channels[index+1:]...)
-				break
-			}
-		}
+		// for index, ch := range client.Channels {
+		// 	if ch == channel {
+		// 		client.Channels = append(client.Channels[:index], client.Channels[index+1:]...)
+		// 		break
+		// 	}
+		// }
+
+		client.Channels = removeFromArray(client.Channels, channel)
 
 		if len(client.Channels) == 0 {
 			delete(cc.clients, id)
 		}
+
+		// NOTE: Debug purposes
+		fmt.Printf("Amount of different clients connected: %d\n", len(cc.clients))
+		for key, value := range cc.clients {
+			value.display(key)
+		}
+		// NOTE: Debug purposes
 	}
 }
 
